2484786f9618c9ae1a87d0904b5eb45e178834c4
DouglasRodrigs <douglassousa.4@hotmail.com>
2024-06-23 11:32:32 -0300

    Incluindo form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ValidatorPasswordTooltipModule } from './validator-password-tooltip/validator-password-tooltip.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule, // add to use FormControl
+    MatInputModule, // add to use input from material
+    ValidatorPasswordTooltipModule, // directive created
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/validator-password-tooltip/validator-password-tooltip.directive.ts
@@ -0,0 +1,47 @@
+import { Directive, Input, OnInit } from '@angular/core';
+import { MatTooltip } from '@angular/material/tooltip';
+
+@Directive({
+  selector: '[validatorTooltip]',
+  exportAs: 'validatorTooltip'
+})
+export class ValidatorPasswordTooltipDirective extends MatTooltip implements OnInit {
+  ngOnInit(): void {
+    super.tooltipClass = 'validator-password-tooltip';
+  }
+
+  @Input()
+  get validatorTooltip() { return this.message; }
+  set validatorTooltip(value: string) {
+    this.message = this.getMessage(value);
+    this.show();
+  }
+  private getMessage(value: string): string {
+    const text = `${this.getTextRoleNumber(this.hasAnyNumber(value))}
+    ${this.getTextRoleUpper(this.hasAnyUpper(value))}
+    ${this.getTextRoleLower(this.hasAnyLower(value))}
+    ${this.getTextRoleSpecial(this.hasAnySpecial(value))}
+    ${this.getTextRoleLimit(this.hasLimit(value))}`;
+    return text;
+  }
+  private getTextRoleNumber(showIcon: boolean): string { return `${this.getIcon(showIcon)} - A senha deve conter pelo menos 1 número (0-9)\n` }
+  private getIcon(showIcon: boolean): string { return `${showIcon ? '✅' : '❌'}` }
+  
+  private hasAnyNumber(value: string): boolean { return /[0-9]/.test(value) }
+  
+  private getTextRoleUpper(showIcon: boolean): string { return `${this.getIcon(showIcon) } - A senha deve conter pelo menos 1 letra maiúscula\n` }
+  
+  private hasAnyUpper(value: string): boolean { return /[A-Z]/.test(value) }
+  
+  private getTextRoleLower(showIcon: boolean): string { return `${this.getIcon(showIcon) } - A senha deve conter pelo menos 1 letra minúscula\n` }
+  
+  private hasAnyLower(value: string): boolean { return /[a-z]/.test(value) }
+  
+  private getTextRoleSpecial(showIcon: boolean): string { return `${this.getIcon(showIcon) } - A senha deve conter pelo menos 1 caractere especial\n` }
+  
+  private hasAnySpecial(value: string): boolean { return /[^\w\d\s:]/.test(value) }
+  
+  private getTextRoleLimit(showIcon: boolean): string { return `${this.getIcon(showIcon)} - A senha deve ter de 8 a 30 caracteres sem espaços\n` }
+  
+  private hasLimit(value: string): boolean { return /^([^\s]){7,20}$/gm.test(value) }
+}
